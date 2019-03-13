b5f0332603a9785f13a5ec414fd02c66dfc6a34a
anfereos <47388168+anfereos@users.noreply.github.com>
2019-03-12 19:42:42 -0500

    Agregar imagenes en web

---
+++ Shop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Http;
+    using Data.Entities;
+
+    public class ProductViewModel : Product
+    {
+        [Display(Name = "image")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
